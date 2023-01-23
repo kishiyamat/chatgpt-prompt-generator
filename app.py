6bdd926bbdb734fcb08b69858fe171ef93782d2b
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2023-01-23 13:31:25 +0900

    最適なpromptはジャンルごとに異なる

--- app.py
+++ app.py
@@ -1,47 +1,6 @@
 import openai
 import streamlit as st
 
-class Request:
-    def __init__(self, request_type: str, pane, request_answers: bool)->None:
-        self.request_type = request_type
-        self.pane = pane
-        self.request_answers = request_answers
-    @property
-    def query(self)->str:
-        base_label = f"N {self.request_type.lower()}"
-        request_answers_str = " with answers" if self.request_answers else ""
-        if self.request_type == "Vocab quizzes":
-            col1, col2 = self.pane.columns(2)
-            n_vocab_quizzes = col1.slider(label=base_label, min_value=2, max_value=6, value=3)
-            m_choice = col2.slider(label= "with M choices", min_value=2, max_value=6, value=4)
-            x_nym = self.pane.radio(label= "X-nyms", options = ["synonym", "antonym"], horizontal=True)
-            # x_nym = self.pane.radio(label= "X-nyms", options = ["synonym", "antonym", "hyponym", "hypernym"], horizontal=True)
-            return f"make {str(n_vocab_quizzes)} vocabulary building quizzes with {m_choice} choices to ask the {x_nym}{request_answers_str}"
-        elif self.request_type == "Difficult words":
-            # explanations は不要
-            options = ["all"]+list(range(3, 21))
-            n_difficult_words = self.pane.select_slider(label=base_label + " (all, 3, 4, 5,...20)", options=options, value="all")
-            return f"find {str(n_difficult_words)} difficult words except for proper nouns"
-        elif self.request_type == "Comprehension tasks":
-            col1, col2 = self.pane.columns(2)
-            n_comprehension = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
-            m_choice = col2.slider(label= "with M choices", min_value=2, max_value=6, value=4)
-            return f"make {str(n_comprehension)} comprehension tasks with {m_choice} choices{request_answers_str}"
-        elif self.request_type == "Discussion topics":
-            col1, col2 = self.pane.columns(2)
-            n_discussion_topics = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
-            m_minutes = col2.slider(label= "with M minutes", min_value=5, max_value=60, value=15, step=5)
-            return f"suggest {str(n_discussion_topics)} discussion topics that is supposed to be finished in {m_minutes} minutes"
-        elif self.request_type == "Word/phrase explanations":
-            words_phrases = self.pane.text_input("Word/phrase")
-            return f'explain the use of "{words_phrases}" and give some other examples'
-        elif self.request_type == "Rewriting":
-            return f'rewrite the text'
-        elif self.request_type == "Summarizing":
-            return f'summarize the text'
-        else:
-            return "Error"
-
 st.header("ChatGPT Prompt Generator")
 st.markdown("""
 This app allows its users...
@@ -50,13 +9,13 @@ This app allows its users...
 """)
 
 target_language = st.sidebar.header("PARAMETERS")
-target_language = "of " + st.sidebar.radio(
+target_language = st.sidebar.radio(
     "Select target language",
     (
         'English',
         'Japanese',
     ))
-reader_student = "for " + st.sidebar.radio(
+reader_student = st.sidebar.radio(
     "Select readers/students level",
     options=(
         'Elementary learners',
@@ -67,6 +26,9 @@ reader_student = "for " + st.sidebar.radio(
         'Highly educated native speakers'
     ),).lower()
 
+subject = reader_student + " of " + target_language
+
+# リクエストごとに異なる最適なクエリを作成する。
 st.sidebar.subheader("Select the request type")
 request_type = st.sidebar.radio("Select request type", (
     "Vocab quizzes",
@@ -78,21 +40,51 @@ request_type = st.sidebar.radio("Select request type", (
     "Summarizing",
 ))
 
-# FIXME: 解答の解説機能があまり機能していない.
 answer_request = st.sidebar.radio("Do you need answers/explanations?", (
     "Yes, please.",
     "No, thank you.",
 ))
 answer_request=answer_request[0]=="Y"
-# answer_request = False
 
-request = Request(request_type, st.sidebar, answer_request).query
+request = ""
+base_label = f"N {request_type.lower()}"
+request_answers_str = " with answers" if answer_request else ""
+
+if request_type == "Vocab quizzes":
+    col1, col2 = st.sidebar.columns(2)
+    n_vocab_quizzes = col1.slider(label=base_label, min_value=2, max_value=6, value=3)
+    m_choice = col2.slider(label= "with M choices", min_value=2, max_value=6, value=4)
+    x_nym = st.sidebar.radio(label= "X-nyms", options = ["synonym", "antonym"], horizontal=True)
+    request = f"make {str(n_vocab_quizzes)} vocabulary building quizzes with {m_choice} choices to ask {subject} the {x_nym}{request_answers_str}"
+elif request_type == "Difficult words":
+    # explanations は不要
+    options = ["all"]+list(range(3, 21))
+    n_difficult_words = st.sidebar.select_slider(label=base_label + " (all, 3, 4, 5,...20)", options=options, value="all")
+    request = f"find {str(n_difficult_words)} difficult words except for proper nouns"
+elif request_type == "Comprehension tasks":
+    col1, col2 = st.sidebar.columns(2)
+    n_comprehension = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
+    m_choice = col2.slider(label= "with M choices", min_value=2, max_value=6, value=4)
+    request = f"make {str(n_comprehension)} comprehension tasks with {m_choice} choices{request_answers_str}"
+elif request_type == "Discussion topics":
+    col1, col2 = st.sidebar.columns(2)
+    n_discussion_topics = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
+    m_minutes = col2.slider(label= "with M minutes", min_value=5, max_value=60, value=15, step=5)
+    request = f"suggest {str(n_discussion_topics)} discussion topics that is supposed to be finished in {m_minutes} minutes"
+elif request_type == "Word/phrase explanations":
+    words_phrases = st.sidebar.text_input("Word/phrase")
+    request = f'explain the use of "{words_phrases}" and give some other examples'
+elif request_type == "Rewriting":
+    request = f'rewrite the text'
+elif request_type == "Summarizing":
+    request = f'summarize the text'
+else:
+    request = "Error"
 
 reference = st.sidebar.radio(
     "Specify the reference",
     ("N/A", 'text')
     )
-
 
 if reference=="N/A":
     reference_txt=f"."
@@ -118,7 +110,7 @@ else:
 # - [ ] 単語数(上限不明)
 # - [ ] 参考文献
 st.subheader("Prompt")
-prompt_a = " ".join(["Please", request, reader_student, target_language])
+prompt_a = " ".join(["Please", request])
 prompt = prompt_a + reference_txt
 st.markdown(prompt + "\n")
 
