e05ab04fb20bcfdce08dbc5df74ccf3ae48fd8af
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2023-01-23 17:44:13 +0900

    大体OK

--- app.py
+++ app.py
@@ -17,15 +17,22 @@ target_language = st.sidebar.radio(
     ))
 reader_student = st.sidebar.radio(
     "Select readers/students level",
+    # FIXME: learners より students のほうが精度良いかも
     options=(
         'Elementary learners',
         'Pre-intermediate learners',
         'Intermediate learners',
         'Advanced learners',
-        'Highly educated native speakers'
+        'Highly educated native speakers',
+        'Students',
     ),).lower()
 
 subject = reader_student + " of " + target_language
+
+answer_request = st.sidebar.radio("Do you need answers/explanations?", (
+    "Yes, please.",
+    "No, thank you.",
+))
 
 # リクエストごとに異なる最適なクエリを作成する。
 st.sidebar.subheader("Select the request type")
@@ -40,10 +47,6 @@ request_type = st.sidebar.radio("Select request type", (
     "Summarizing",
 ))
 
-answer_request = st.sidebar.radio("Do you need answers/explanations?", (
-    "Yes, please.",
-    "No, thank you.",
-))
 answer_request=answer_request[0]=="Y"
 
 request = ""
@@ -85,17 +88,17 @@ elif request_type == "Discussion topics":
     col1, col2 = st.sidebar.columns(2)
     n_discussion_topics = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
     m_minutes = col2.slider(label= "with M minutes", min_value=5, max_value=60, value=15, step=5)
-    request = f"suggest {n_discussion_topics} discussion topics that {subject} is supposed to finish in {m_minutes} minutes"
+    request = f"suggest {n_discussion_topics} discussion topics that {subject} are supposed to finish in {m_minutes} minutes"
 elif request_type=="Cloze tests":
     # - [ ] subject
     # - [x] answer_request
     col1, col2 = st.sidebar.columns(2)
     n_cloze_test = col1.slider(label=base_label, min_value=3, max_value=10, value=3)
     m_choice = col2.slider(label= "with M choices", min_value=2, max_value=6, value=4)
-    pos = st.sidebar.radio(label= "PoS", options = ["preposition", "article"], horizontal=True)
+    pos = st.sidebar.radio(label= "PoS", options = ["preposition", "noun", "adjective", "verb"], horizontal=True)
     # FIXME: students -> learners に変えてみる
     request = " ".join([
-        f"make {n_cloze_test} cloze tests, each of which has a blank that students are supposed to fill in",
+        f"make {n_cloze_test} cloze tests, each of which has a blank that {subject} are supposed to fill in",
         f"selecting a correct {pos} from {m_choice} choices"
     ]) 
 elif request_type == "Word/phrase explanations":
@@ -140,26 +143,28 @@ if st.button('Submit to InstructGPT'):
     st.info(" ".join([
         "The following output is generated using InstructGPT, a precedent model of ChatGPT.",
         "The response might not be complete due to the lack of computational resources."
-        "It may cost a couple of time to get a ready-to-use response.",
+        "It may cost a couple of time to get a ready-to-use response, even with the ChatGPT.",
         "You can regenerate the responses from ChatGPT on the latest application.",
         ])
     )
 
-    openai.api_key = st.secrets["OPENAI_TOKEN"]
-    response = openai.Completion.create(
+    api_params = dict(
         model='text-davinci-003',  # InstructGPT
-        prompt=prompt,
-        temperature=0.5,  # synonym は0.5くらいが結構よさそう
+        # temperature=0.5,  # synonym は0.5くらいが結構よさそう
+        temperature=0.3,
+        # temperature=0.7,
         # temperature=0.9,
-        # max_tokens= 1024,  # 
-        max_tokens=512,  # これでも1段落で8問程度はいける
+        max_tokens= 1024,  # 
+        # max_tokens=512,  # これでも1段落で8問程度はいける
         # max_tokens=256,  # これだと3問程度
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0
+        presence_penalty=0,
     )
-
-    st.code(response['choices'][0]['text'])
+    openai.api_key = st.secrets["OPENAI_TOKEN"]
+    response = openai.Completion.create(**api_params, prompt=prompt)
+    st.code(response['choices'][0]['text'], language=None)
+    st.code(str(api_params), language=None)
     # st.markdown(body=(response['choices'][0]['text']).replace("\n", "\n\n"))
 
 st.sidebar.markdown("If you have any questions, please email the following address.")
